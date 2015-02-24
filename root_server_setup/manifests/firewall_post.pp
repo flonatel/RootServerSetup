d9285cc78dd4d00e21c1cbfc3ac9566514b1e87d
Andreas Florath <andreas@florath.net>
2015-02-24 15:00:05 +0100

    Added IPv6 log and drop rules

--- root_server_setup/manifests/firewall_post.pp
+++ root_server_setup/manifests/firewall_post.pp
@@ -1,6 +1,8 @@
 class root_server_setup::firewall_post {
-  
-  firewall { '900 log dropped input chain':
+
+  # Duplicate entries for IPv4 and IPv6
+  firewall { '900 IPv4 log dropped input chain':
+    provider   => 'iptables',
     chain      => 'INPUT',
     jump       => 'LOG',
     log_level  => '6',
@@ -9,7 +11,19 @@ class root_server_setup::firewall_post {
     before     => undef,
   }
 
-  firewall { '900 log dropped forward chain':
+  firewall { '900 IPv6 log dropped input chain':
+    provider   => 'ip6tables',
+    chain      => 'INPUT',
+    jump       => 'LOG',
+    log_level  => '6',
+    log_prefix => '[IPTABLES INPUT] dropped ',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  # Duplicate entries for IPv4 and IPv6
+  firewall { '900 IPv4 log dropped forward chain':
+    provider   => 'iptables',
     chain      => 'FORWARD',
     jump       => 'LOG',
     log_level  => '6',
@@ -18,7 +32,19 @@ class root_server_setup::firewall_post {
     before     => undef,
   }
 
-  firewall { '900 log dropped output chain':
+  firewall { '900 IPv6 log dropped forward chain':
+    provider   => 'ip6tables',
+    chain      => 'FORWARD',
+    jump       => 'LOG',
+    log_level  => '6',
+    log_prefix => '[IPTABLES FORWARD] dropped ',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  # Duplicate entries for IPv4 and IPv6
+  firewall { '900 IPv4 log dropped output chain':
+    provider   => 'iptables',
     chain      => 'OUTPUT',
     jump       => 'LOG',
     log_level  => '6',
@@ -27,21 +53,61 @@ class root_server_setup::firewall_post {
     before     => undef,
   }
 
-  firewall { "910 deny all other input requests":
+  firewall { '900 IPv6 log dropped output chain':
+    provider   => 'ip6tables',
+    chain      => 'OUTPUT',
+    jump       => 'LOG',
+    log_level  => '6',
+    log_prefix => '[IPTABLES OUTPUT] dropped ',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  # Duplicate entries for IPv4 and IPv6
+  firewall { "910 IPv4 deny all other input requests":
+    provider   => 'iptables',
     chain      => 'INPUT',
     action     => 'drop',
     proto      => 'all',
     before     => undef,
   }
 
-  firewall { "910 deny all other forward requests":
+  firewall { "910 IPv6 deny all other input requests":
+    provider   => 'ip6tables',
+    chain      => 'INPUT',
+    action     => 'drop',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  # Duplicate entries for IPv4 and IPv6
+  firewall { "910 IPv4 deny all other forward requests":
+    provider   => 'iptables',
     chain      => 'FORWARD',
     action     => 'drop',
     proto      => 'all',
     before     => undef,
   }
 
-  firewall { "910 deny all other output requests":
+  firewall { "910 IPv6 deny all other forward requests":
+    provider   => 'ip6tables',
+    chain      => 'FORWARD',
+    action     => 'drop',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  # Duplicate entries for IPv4 and IPv6
+  firewall { "910 IPv4 deny all other output requests":
+    provider   => 'iptables',
+    chain      => 'OUTPUT',
+    action     => 'drop',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  firewall { "910 IPv6 deny all other output requests":
+    provider   => 'ip6tables',
     chain      => 'OUTPUT',
     action     => 'drop',
     proto      => 'all',
